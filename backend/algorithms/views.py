f9280fe1e10cca2f56aa18121eb401112b21a9fd
Sarthakischauhan <sarthakchauhann@gmail.com>
2025-03-10 02:09:03 -0700

    logic changes and constants

--- backend/algorithms/views.py
+++ backend/algorithms/views.py
@@ -1,17 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from bioapp.constants import MRNA_TABLE, RNA_CODON_TABLE
 
 # Transcription Algorithm
 @api_view(['POST'])
 def transcribe_dna(request):
     dna = request.data.get('dna', '').upper()
-    dna.replace("\n","")
+    dna = dna.replace("\n","")
     if not all(ncltide in "ATCG" for ncltide in dna):
-        return Response({"message":"Bad request: Can only have ATCG nucleotide"}, status=400)
-
-    mrna_table = {"C":"G", "G":"C", "T":"A", "A":"U"}
-    mrna = "".join([mrna_table[nctide] for nctide in dna])
+        return Response({"error":"Bad request: Can only have ATCG nucleotide"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    mrna = "".join([MRNA_TABLE[nctide] for nctide in dna])
 
     return Response({"mrna":mrna}, status=200)
 
@@ -19,18 +19,15 @@ def transcribe_dna(request):
 @api_view(['POST'])
 def translate_mrna(request):
     mrna = request.data.get('mrna', '').upper()
-    if not all(base in 'AUCG' for base in mrna) or len(mrna) % 3 != 0:
+    mrna = mrna.replace("\n", "")
+    if not all(base in 'AUCG' for base in mrna):
         return Response({"error": "Invalid mRNA sequence"}, status=status.HTTP_400_BAD_REQUEST)
     
-    codon_table = {
-        "AUG": "Met", "UUU": "Phe", "UUC": "Phe", "UUA": "Leu", "UUG": "Leu",
-        "UAA": "Stop", "UAG": "Stop", "UGA": "Stop" 
-    }
     protein = []
     for i in range(0, len(mrna), 3):
         codon = mrna[i:i+3]
-        if codon in codon_table:
-            aa = codon_table[codon]
+        if codon in RNA_CODON_TABLE:
+            aa = RNA_CODON_TABLE[codon]
             if aa == "Stop":
                 break
             protein.append(aa)
